26e1038244938571546cf3f69669bcd24237e491
stonehuang <stonehuang@canway.net>
2023-07-21 15:50:33 +0800

    feature: add single

--- write_through_test.go
+++ write_through_test.go
@@ -1,1 +1,28 @@
 package cache
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func ExampleNewWriteThroughCache() {
+	c, err := NewLocalCache()
+	if err != nil {
+		panic(err)
+	}
+	wtc, err := NewWriteThroughCache(c, func(ctx context.Context, key string, val any) error {
+		fmt.Printf("write data to somewhere key %s, val %v \n", key, val)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = wtc.Set(context.Background(),
+		"/biz/user/id=1", "I am user 1", time.Minute)
+	if err != nil {
+		panic(err)
+	}
+	// Output:
+	// write data to somewhere key /biz/user/id=1, val I am user 1
+}
