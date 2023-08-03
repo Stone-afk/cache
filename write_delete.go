6a358f91f6d9bb1ae6b91fcede3adc0f9f922a3c
stonehuang <stonehuang@canway.net>
2023-08-03 13:45:30 +0800

    feature: add test

--- write_delete.go
+++ write_delete.go
@@ -1,8 +1,25 @@
 package cache
 
-import "context"
+import (
+	"cache/internal/errs"
+	"context"
+)
 
 type WriteDeleteCache struct {
 	Cache
 	storeFunc func(ctx context.Context, key string, val any) error
+}
+
+func NewWriteDeleteCache(cache Cache, fn func(ctx context.Context, key string, val any) error) (*WriteDeleteCache, error) {
+	if fn == nil {
+		return nil, errs.ErrStoreFuncRequired
+	}
+	if cache == nil {
+		return nil, errs.ErrCacheRequired
+	}
+
+	return &WriteDeleteCache{
+		Cache:     cache,
+		storeFunc: fn,
+	}, nil
 }
