ab08b7242f85ea8051f668d8cd4ba0fbbde5b700
stonehuang <stonehuang@canway.net>
2023-08-04 13:26:35 +0800

    feature: add test

--- write_delete_test.go
+++ write_delete_test.go
@@ -3,10 +3,134 @@ package cache
 import (
 	"cache/internal/errs"
 	"context"
+	"errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
+
+func TestWriteDeleteCache_Set(t *testing.T) {
+	mockDbStore := make(map[string]any)
+
+	cancels := make([]func(), 0)
+	defer func() {
+		for _, cancel := range cancels {
+			cancel()
+		}
+	}()
+
+	testCases := []struct {
+		name      string
+		cache     Cache
+		storeFunc func(ctx context.Context, key string, val any) error
+		ctx       context.Context
+		key       string
+		value     any
+		wantErr   error
+		before    func(Cache)
+		after     func()
+	}{
+		{
+			name:  "store key/value in db fail",
+			key:   "hello",
+			value: "world",
+			cache: func() Cache {
+				c, err := NewLocalCache()
+				assert.Nil(t, err)
+				return c
+			}(),
+			storeFunc: func(ctx context.Context, key string, val any) error {
+				return errors.New("failed")
+			},
+			ctx: context.TODO(),
+			wantErr: func() error {
+				err := errors.New("failed")
+				wrapErr := errors.New(fmt.Sprintf("%s, %s", err.Error(), fmt.Sprintf("key: hello, val: world")))
+				return errs.ErrStoreFailed(wrapErr)
+			}(),
+			before: func(cache Cache) {},
+			after:  func() {},
+		},
+		{
+			name: "store key/value success",
+			cache: func() Cache {
+				c, err := NewLocalCache()
+				assert.Nil(t, err)
+				return c
+			}(),
+			storeFunc: func(ctx context.Context, key string, val any) error {
+				mockDbStore[key] = val
+				return nil
+			},
+			ctx:   context.TODO(),
+			key:   "hello",
+			value: "world",
+			before: func(cache Cache) {
+				_ = cache.Set(context.Background(), "hello", "testVal", 10*time.Second)
+			},
+			after: func() {
+				delete(mockDbStore, "hello")
+			},
+		},
+		{
+			name: "store key/value timeout",
+			cache: func() Cache {
+				c, err := NewLocalCache()
+				assert.Nil(t, err)
+				return c
+			}(),
+			storeFunc: func(ctx context.Context, key string, val any) error {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case <-time.After(3 * time.Second):
+					mockDbStore[key] = val
+					return nil
+				}
+
+			},
+			ctx: func() context.Context {
+				ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+				cancels = append(cancels, cancel)
+				return ctx
+
+			}(),
+			key:   "hello",
+			value: nil,
+			before: func(cache Cache) {
+				_ = cache.Set(context.Background(), "hello", "testVal", 10*time.Second)
+			},
+			after: func() {},
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			w, err := NewWriteDeleteCache(tt.cache, tt.storeFunc)
+			if err != nil {
+				assert.EqualError(t, tt.wantErr, err.Error())
+				return
+			}
+
+			tt.before(tt.cache)
+			defer func() {
+				tt.after()
+			}()
+
+			err = w.Set(tt.ctx, tt.key, tt.value)
+			if err != nil {
+				assert.EqualError(t, tt.wantErr, err.Error())
+				return
+			}
+
+			_, err = w.Get(tt.ctx, tt.key)
+			assert.Equal(t, errs.ErrKeyNotFound, err)
+
+			vv := mockDbStore[tt.key]
+			assert.Equal(t, tt.value, vv)
+		})
+	}
+}
 
 func TestNewWriteDeleteCache(t *testing.T) {
 	underlyingCache, err := NewLocalCache()
