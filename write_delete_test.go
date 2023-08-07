ff164a7382f95cf81cee5d963e8bdc5796283e96
stonehuang <stonehuang@canway.net>
2023-08-07 12:45:26 +0800

    feature: add test

--- write_delete_test.go
+++ write_delete_test.go
@@ -10,6 +10,27 @@ import (
 	"time"
 )
 
+func ExampleWriteDoubleDeleteCache() {
+	c, err := NewLocalCache()
+	if err != nil {
+		panic(err)
+	}
+	wtc, err := NewWriteDoubleDeleteCache(c, 1*time.Second, 3*time.Second, func(ctx context.Context, key string, val any) error {
+		fmt.Printf("write data to somewhere key %s, val %v \n", key, val)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = wtc.Set(context.Background(),
+		"/biz/user/id=1", "I am user 1")
+	if err != nil {
+		panic(err)
+	}
+	// Output:
+	// write data to somewhere key /biz/user/id=1, val I am user 1
+}
+
 func TestWriteDeleteCache_Set(t *testing.T) {
 	mockDbStore := make(map[string]any)
 
