a58b282c1613226ec65e5fd762b06c190ddfdda2
stonehuang <stonehuang@canway.net>
2023-08-10 19:14:23 +0800

    feature: add test

--- write_through_test.go
+++ write_through_test.go
@@ -3,11 +3,68 @@ package cache
 import (
 	"cache/internal/errs"
 	"context"
+	"errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
+
+func TestWriteThoughCache_Set(t *testing.T) {
+	mockDbStore := make(map[string]any)
+
+	testCases := []struct {
+		name      string
+		cache     Cache
+		storeFunc func(ctx context.Context, key string, val any) error
+		key       string
+		value     any
+		wantErr   error
+	}{
+		{
+			name:  "store key/value in db fail",
+			cache: NewMemoryCache(),
+			storeFunc: func(ctx context.Context, key string, val any) error {
+				return errors.New("failed")
+			},
+			wantErr: berror.Wrap(errors.New("failed"), PersistCacheFailed,
+				fmt.Sprintf("key: %s, val: %v", "", nil)),
+		},
+		{
+			name:  "store key/value success",
+			cache: NewMemoryCache(),
+			storeFunc: func(ctx context.Context, key string, val any) error {
+				mockDbStore[key] = val
+				return nil
+			},
+			key:   "hello",
+			value: "world",
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			w, err := NewWriteThroughCache(tt.cache, tt.storeFunc)
+			if err != nil {
+				assert.EqualError(t, tt.wantErr, err.Error())
+				return
+			}
+
+			err = w.Set(context.Background(), tt.key, tt.value, 60*time.Second)
+			if err != nil {
+				assert.EqualError(t, tt.wantErr, err.Error())
+				return
+			}
+
+			val, err := w.Get(context.Background(), tt.key)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.value, val)
+
+			vv, ok := mockDbStore[tt.key]
+			assert.True(t, ok)
+			assert.Equal(t, tt.value, vv)
+		})
+	}
+}
 
 func TestNewWriteThoughCache(t *testing.T) {
 	underlyingCache, err := NewLocalCache()
